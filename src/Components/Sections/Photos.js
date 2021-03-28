3837c8129dfe35fe00d43f8d055240d66fb51661
Gamma <37131904+versiongamma@users.noreply.github.com>
2021-03-29 11:52:27 +1300

    Update Photos.js

--- src/Components/Sections/Photos.js
+++ src/Components/Sections/Photos.js
@@ -4,33 +4,25 @@ import { useEffect, useState } from "react";
 
 import image from './../../img/DSCF4873.jpg';
 
-function importAll(r) { return r.keys().map(); }
-const images = importAll(require.context('./../../img/', false, /\.(png|jpe?g|svg)$/));
-
 export default function Photos(props) {
     const [visible, setVisible] = useState(false);
 
     useEffect(() => {
         setVisible(true);
-        images.map(image => console.log(image));
-        console.log(image)
     }, []);
 
     return (
         <Fade in={visible}>
             <Container maxWidth="xl">
                 <Grid container spacing={3}>
-                    {images.map(img => (
-                        <Grid item xl={4}>
-                            <Card style={{ minWidth: 500, margin: 20 }}>
-                                <CardMedia
-                                    style={{ height: 0, paddingTop: '56.25%' }}
-                                    image={img}
-                                />
-                            </Card>
-                        
+                    <Grid item xl={4}>
+                        <Card style={{ minWidth: 500, margin: 20 }}>
+                            <CardMedia
+                                style={{ height: 0, paddingTop: '56.25%' }}
+                                image={image}
+                            />
+                        </Card>
                     </Grid>
-                    ))}
                 </Grid>
             </Container>
         </Fade>
