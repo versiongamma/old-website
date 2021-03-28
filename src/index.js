a8c5001cf215b9a6544607e22bbe94a8352a359e
Gamma <37131904+versiongamma@users.noreply.github.com>
2021-03-29 12:14:13 +1300

    Create placeholder site for deployment

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import { CssBaseline } from '@material-ui/core';
+import { CssBaseline, ThemeProvider } from '@material-ui/core';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App';
+import theme from './theme';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme}>
     <CssBaseline>
       <App />
     </CssBaseline>
-  </React.StrictMode>,
+  </ThemeProvider>,
   document.getElementById('root')
 );
