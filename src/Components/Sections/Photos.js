62cc8f3247cd22b5d6a75aed566e03d31d8118e3
Gamma <37131904+versiongamma@users.noreply.github.com>
2021-03-29 21:11:28 +1300

    Proper display of stored photos

--- src/Components/Sections/Photos.js
+++ src/Components/Sections/Photos.js
@@ -1,29 +1,39 @@
 import { Container, Grid, Card, CardMedia, Fade } from "@material-ui/core";
+import { Skeleton } from '@material-ui/lab';
 import { useEffect, useState } from "react";
 
-import image from './../../img/DSCF4873.jpg';
+import image from './../../img/Wallpaper.jpg'
 
 export default function Photos(props) {
-    const [visible, setVisible] = useState(false);
+  const importAll = (r) => { return r.keys().map(r); }
+  const images = importAll(require.context('./../../img/', false, /\.(png|jpe?g|svg)$/));
 
-    useEffect(() => {
-        setVisible(true);
-    }, []);
+  const [visible, setVisible] = useState(false);
 
-    return (
-        <Fade in={visible}>
-            <Container maxWidth="xl">
-                <Grid container spacing={3}>
-                    <Grid item xl={4}>
-                        <Card style={{ minWidth: 500, margin: 20 }}>
-                            <CardMedia
-                                style={{ height: 0, paddingTop: '56.25%' }}
-                                image={image}
-                            />
-                        </Card>
-                    </Grid>
-                </Grid>
-            </Container>
-        </Fade>
-    );
+  useEffect(() => {
+    setVisible(true);
+  }, []);
+
+  return (
+    <Fade in={visible}>
+      <Container maxWidth="xl" style={{textAlign: 'center'}}>
+          {images.map((img) => (
+              <img 
+                src={img.default} 
+                style={{width: '20vw', padding: 40}}
+              />
+          ))}
+      </Container>
+    </Fade>
+  );
 }
+
+/**
+ * <Card style={{ minWidth: 500, margin: 20 }}>
+                <CardMedia
+                  style={{ height: 0, paddingTop: '56.25%' }}
+                  image={img.default}
+                />
+              </Card>
+ * 
+ */
