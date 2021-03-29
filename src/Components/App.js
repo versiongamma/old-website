505c8fa50c0ab3ad1d65edf29a176e9bef81ba9c
Gamma <37131904+versiongamma@users.noreply.github.com>
2021-03-29 23:56:33 +1300

    Added top bar

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,41 +1,57 @@
-import { BottomNavigation, BottomNavigationAction, Box, CssBaseline, ThemeProvider} from '@material-ui/core';
+import { BottomNavigation, BottomNavigationAction, Box, CssBaseline, ThemeProvider, AppBar, Toolbar, Fade } from '@material-ui/core';
 import PhotoIcon from '@material-ui/icons/Photo';
 import VideocamIcon from '@material-ui/icons/Videocam';
 import GamesIcon from '@material-ui/icons/Games';
 import InfoIcon from '@material-ui/icons/Info';
 import ComputerIcon from '@material-ui/icons/Computer';
 import SettingsIcon from '@material-ui/icons/Settings';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import Photos from './Sections/Photos';
 import Settings from './Sections/Settings';
 
 import lightTheme from './../themes/lightTheme';
 import darkTheme from './../themes/darkTheme';
- 
+
 export default function App() {
   const updateTheme = () => {
     setTheme(prev => prev == 'lightTheme' ? 'darkTheme' : 'lightTheme');
-    setSettings(prev => ({...prev, themeSelector: !prev.themeSelector}))
+    setSettings(prev => ({ ...prev, themeSelector: !prev.themeSelector }))
   }
 
   const [settings, setSettings] = useState({
     themeSelector: false
   });
-  const [activeSection, setActiveSection] = useState(0); 
+
+  const [activeSection, setActiveSection] = useState(0);
   const [theme, setTheme] = useState('lightTheme');
-  const sections = [<br />, <br />, <br />, <Photos />, <br />, <Settings updateTheme={updateTheme} settings={settings}/>];
+  const [visible, setVisible] = useState(false)
+  const sections = [<br />, <br />, <br />, <Photos />, <br />, <Settings updateTheme={updateTheme} settings={settings} />];
+
+  useEffect(() => {
+    setVisible(true);
+  },[])
 
   return (
     <ThemeProvider theme={theme == 'lightTheme' ? lightTheme : darkTheme}>
       <CssBaseline>
-        <Box>
+        <Fade in={visible}>
+          <AppBar position='fixed' style={{ alignItems: 'center' }}>
+            <Toolbar>
+              <img
+                src='http://i.imgur.com/u5vk60X.jpg'
+                style={{ width: '15vw' }} />
+            </Toolbar>
+          </AppBar>
+        </Fade>
+        <Fade in={visible}>
+        <Box style={{ paddingTop: '12vh' }}>
           {sections[activeSection]}
-          <BottomNavigation 
-            showLabels 
+          <BottomNavigation
+            showLabels
             value={activeSection}
-            onChange={(event, value) => {setActiveSection(value)}}
-            style={{width: '100%', position: 'fixed', bottom: 0, backgroundColor: theme == 'darkTheme' ? '#303030' : '#fff'}}
+            onChange={(event, value) => { setActiveSection(value) }}
+            style={{ width: '100%', position: 'fixed', bottom: 0, backgroundColor: theme == 'darkTheme' ? '#303030' : '#fff' }}
           >
             <BottomNavigationAction icon={<InfoIcon />} label="About" />
             <BottomNavigationAction icon={<GamesIcon />} label="Game Design" />
@@ -45,6 +61,7 @@ export default function App() {
             <BottomNavigationAction icon={<SettingsIcon />} label="Settings" />
           </BottomNavigation>
         </Box>
+        </Fade>
       </CssBaseline>
     </ThemeProvider>
   );
