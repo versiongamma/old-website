903011021d3d05a8e0d6ce300b4340c80f80ebf8
Gamma <37131904+versiongamma@users.noreply.github.com>
2021-03-30 00:14:35 +1300

    Add placeholder sections

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,4 +1,4 @@
-import { BottomNavigation, BottomNavigationAction, Box, CssBaseline, ThemeProvider, AppBar, Toolbar, Fade } from '@material-ui/core';
+import { BottomNavigation, BottomNavigationAction, Box, CssBaseline, ThemeProvider, AppBar, Toolbar, Fade, Paper, Divider } from '@material-ui/core';
 import PhotoIcon from '@material-ui/icons/Photo';
 import VideocamIcon from '@material-ui/icons/Videocam';
 import GamesIcon from '@material-ui/icons/Games';
@@ -7,8 +7,13 @@ import ComputerIcon from '@material-ui/icons/Computer';
 import SettingsIcon from '@material-ui/icons/Settings';
 import { useEffect, useState } from 'react';
 
+import About from './Sections/About';
+import GameDev from './Sections/GameDev';
+import Software from './Sections/Software';
 import Photos from './Sections/Photos';
+import Videos from './Sections/Videos';
 import Settings from './Sections/Settings';
+
 
 import lightTheme from './../themes/lightTheme';
 import darkTheme from './../themes/darkTheme';
@@ -26,11 +31,11 @@ export default function App() {
   const [activeSection, setActiveSection] = useState(0);
   const [theme, setTheme] = useState('lightTheme');
   const [visible, setVisible] = useState(false)
-  const sections = [<br />, <br />, <br />, <Photos />, <br />, <Settings updateTheme={updateTheme} settings={settings} />];
+  const sections = [<About />, <GameDev />, <Software />, <Photos />, <Videos />, <Settings updateTheme={updateTheme} settings={settings} />];
 
   useEffect(() => {
     setVisible(true);
-  },[])
+  }, [])
 
   return (
     <ThemeProvider theme={theme == 'lightTheme' ? lightTheme : darkTheme}>
@@ -40,27 +45,33 @@ export default function App() {
             <Toolbar>
               <img
                 src='http://i.imgur.com/u5vk60X.jpg'
-                style={{ width: '15vw' }} />
+                style={{ width: '15vw', padding: '0.5vh' }} />
             </Toolbar>
           </AppBar>
         </Fade>
         <Fade in={visible}>
-        <Box style={{ paddingTop: '12vh' }}>
-          {sections[activeSection]}
-          <BottomNavigation
-            showLabels
-            value={activeSection}
-            onChange={(event, value) => { setActiveSection(value) }}
-            style={{ width: '100%', position: 'fixed', bottom: 0, backgroundColor: theme == 'darkTheme' ? '#303030' : '#fff' }}
-          >
-            <BottomNavigationAction icon={<InfoIcon />} label="About" />
-            <BottomNavigationAction icon={<GamesIcon />} label="Game Design" />
-            <BottomNavigationAction icon={<ComputerIcon />} label="Software Development" />
-            <BottomNavigationAction icon={<PhotoIcon />} label="Photography" />
-            <BottomNavigationAction icon={<VideocamIcon />} label="Videography" />
-            <BottomNavigationAction icon={<SettingsIcon />} label="Settings" />
-          </BottomNavigation>
-        </Box>
+          <Box style={{ paddingTop: '12vh' }}>
+            {sections[activeSection]}
+
+            <BottomNavigation
+              showLabels
+              value={activeSection}
+              onChange={(event, value) => { setActiveSection(value) }}
+              style={{ 
+                width: '100%', 
+                position: 'fixed', 
+                bottom: 0, 
+                backgroundColor: theme == 'darkTheme' ? '#303030' : '#fff' }}
+            >
+              <BottomNavigationAction icon={<InfoIcon />} label="About" />
+              <BottomNavigationAction icon={<GamesIcon />} label="Game Design" />
+              <BottomNavigationAction icon={<ComputerIcon />} label="Software Development" />
+              <BottomNavigationAction icon={<PhotoIcon />} label="Photography" />
+              <BottomNavigationAction icon={<VideocamIcon />} label="Videography" />
+              <BottomNavigationAction icon={<SettingsIcon />} label="Settings" />
+            </BottomNavigation>
+
+          </Box>
         </Fade>
       </CssBaseline>
     </ThemeProvider>
---
+++ src/Components/Sections/About.js
@@ -0,0 +1,18 @@
+import { Box, Fade, Typography } from '@material-ui/core'
+import { useEffect, useState } from 'react';
+
+export default function About(props) {
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    setVisible(true);
+  },[]);
+
+  return (
+    <Fade in={visible}>
+    <Box>
+      <Typography variant="h1" style={{textAlign: "center", paddingTop: "20%", fontFamily: "Josefin Sans"}}>Coming Soon...</Typography>
+    </Box>
+    </Fade>
+  );
+}
