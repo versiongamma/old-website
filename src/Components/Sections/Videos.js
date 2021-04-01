ea5fe44f9246d361d6f901e721fbbc5a1b8fa848
Gamma <37131904+versiongamma@users.noreply.github.com>
2021-04-01 19:25:08 +1300

    Revert "Test videos page"
    This reverts commit b53c0f5ea65f78bf97ad37970832b04fcb302a96.

--- src/Components/Sections/Videos.js
+++ src/Components/Sections/Videos.js
@@ -1,35 +1,18 @@
-import { Container, Divider, Fade, Typography } from '@material-ui/core'
+import { Box, Fade, Typography } from '@material-ui/core'
 import { useEffect, useState } from 'react';
-import ReactPlayer from 'react-player';
-import useWindowSize from '../../hooks/useWindowSize';
 
 export default function Videos(props) {
   const [visible, setVisible] = useState(false);
-  const windowSize = useWindowSize();
 
   useEffect(() => {
     setVisible(true);
-    console.log(windowSize)
-  }, []);
+  },[]);
 
   return (
     <Fade in={visible}>
-      <Container maxWidth='md'>
-        <Typography variant='h4' style={{ fontFamily: 'Josefin Sans', textAlign: 'center' }}>Title of the fuckin video</Typography>
-        <Typography>Description or some shit idk</Typography>
-        <ReactPlayer url='https://www.youtube.com/watch?v=QjvzUMy3zF0' controls
-          width={windowSize.width >= 960 ? 960 : '90vw'}
-          height={windowSize.width >= 960 ? 540 : windowSize.width * .9 * (9 / 16)}
-        />
-
-        <Divider style={{ margin: 50 }} />
-        <Typography variant='h4' style={{ fontFamily: 'Josefin Sans', textAlign: 'center' }}>Title of the fuckin video</Typography>
-        <Typography>Description or some shit idk</Typography>
-        <ReactPlayer url='https://www.youtube.com/watch?v=QjvzUMy3zF0' controls
-          width={windowSize.width >= 960 ? 960 : '90vw'}
-          height={windowSize.width >= 960 ? 540 : windowSize.width * .9 * (9 / 16)} />
-        <Divider style={{ margin: 50 }} />
-      </Container>
+    <Box>
+      <Typography variant="h1" style={{textAlign: "center", paddingTop: "20%", fontFamily: "Josefin Sans"}}>Coming Soon...</Typography>
+    </Box>
     </Fade>
   );
 }
